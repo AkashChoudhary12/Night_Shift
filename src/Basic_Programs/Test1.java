bbd04d9c2f22807d05f73aa156f9c2aa18219723
HP <HP@LAPTOP-BU5UP52J>
2022-07-01 14:02:58 +0530

    commmiting test2 branchX

---
+++ src/Basic_Programs/Test1.java
@@ -0,0 +1,21 @@
+package Basic_Programs;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	//kjsbsbfbsbfbsbfbsk	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
